9fd7c973db0928a79acb4fce629e31d018f9ce8c
Borys Hunko <borys.hunko11@gmail.com>
2021-04-06 21:57:07 +0300

    added method to test size() method after initialization

--- src/main/test/MyListTest.java
+++ src/main/test/MyListTest.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import task1.collection.Product;
+import task1.entities.MyList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyListTest {
+    private MyList<Product> products;
+
+    @BeforeEach
+    void initProducts() {
+        products = new MyList<>();
+    }
+
+    @Test
+    @DisplayName("test size() after initialization")
+    void testSizeAfterInit(){
+        assertEquals(0,products.size(),"should return 0");
+    }
 }
